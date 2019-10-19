5b7dd1c70672e189fbc479d92ce7e58984bade3b
GiaiTran <39039933+GiaiTran@users.noreply.github.com>
2019-10-19 14:35:31 -0500

    Update NeuralNetwork.py
    Change read csv and update KFold load each trainging/testing set into dataframe for training

--- Assignment_2/NeuralNetwork.py
+++ Assignment_2/NeuralNetwork.py
@@ -9,38 +9,25 @@ from sklearn.preprocessing import MinMaxScaler
 # from sklearn.neural_network import
 
 #Load dataset into pandas dataframe
-dataset = pd.read_csv('cmc.data ')
-dataset.columns = ['wife_age', 'wife_education', 'husband_education', 'number_of_children', 'wife_religion',
-              'is_wife_working', 'husband_occupation', 'standard_of_living',
-              'media_exposure', 'contraceptive_method_used']
+dataset = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/cmc/cmc.data', header = None)
+# print(dataset)
+
+#def correlation_heatmap(dataset):
+correlations = dataset.corr()
+plt.figure(figsize=(10, 13))
+sns.heatmap(correlations, vmax=1.0, center=0, fmt='.2f',
+            square=False, linewidths=.5, annot=True, cbar_kws={'shrink': .70},
+            cmap='coolwarm')
+# plt.show();
 
 #indicates that all the rows of column index 0-12 are considered as features
 # and the column with the index 13 to be the dependent variable
 X = dataset.iloc[:, [0, 8]]
 Y = dataset.iloc[:, 9]
 
-
 #Preprocessing step: re-scales data between a specific range 0 and 1
 scaler = MinMaxScaler(feature_range=(0, 1))
 X = scaler.fit_transform(X)
-# print(dataset)
-
-
-# Correlation heatmap
-# For checking the correlation of each feature with the class feature
-def correlation_heatmap(data):
-    correlations = data.corr()
-    # plt.subplots(figsize=(9, 9))
-    plt.figure(figsize=(10, 13))
-    sns.heatmap(correlations, vmax=1.0, center=0, fmt='.2f',
-                square=False, linewidths=.5, annot=True, cbar_kws={'shrink': .70},
-                cmap='coolwarm')
-    plt.show();
-
-
-# correlation_heatmap(dataset)
-
-
 
 # Check dataset balance or not
 # If dataset is not balance and training produces a low accuracy,
@@ -49,7 +36,7 @@ def correlation_heatmap(data):
 class1 = 0
 class2 = 0
 class3 = 0
-for row in dataset['contraceptive_method_used']:
+for row in dataset.iloc[:, 9]:
     if row == 1:
         class1 +=1
     elif row == 2:
@@ -62,23 +49,11 @@ print('Class 2: ', class2)
 print('Class 3: ', class3)
 print('Total row: ', class1 + class2 + class3)
 
-
-# Prepare cross validation
-# Split dataset into 10: 9 training + 1 test set each.
-# KFold configuration: split 10, shuffle = True, seed = 1.
-class1 = 0
-class2 = 0
-class3 = 0
-fk = KFold(10, True, 1)
-test1 = []
-for train_index, test_index in fk.split(dataset):
-    # print(type(train_index))
-    # print("Train Index: ", train_index, '\n')
-    # print("Test Index ", test_index, '\n')
-    test1.clear()
-    for row in np.nditer(train_index):
-        test1.append(row)
-
-    for r in test1:
-        print(type(dataset.iloc[r]))
+#Use KFold to split dataset into 10 subset which includes 1 training and 1 testing set
+kf = KFold(n_splits=10, random_state=1, shuffle=True)
+for train_index, test_index in kf.split(dataset):
+#Loop through each training/testing set and load into dataframe corresponding, then start training
+    df_train = pd.DataFrame(dataset, index = train_index)
+    df_test = pd.DataFrame(dataset, index = test_index)
+    print(df_test)
 
